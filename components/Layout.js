77021f63132c7f67f64d9c3771477a057c185aae
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2023-01-13 18:55:27 +0530

    next init

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+import Nav from "./Nav";
+import styles from "../styles/Layout.module.css";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Nav  />
+      <div className={styles.container}>
+        <main className={styles.main}>{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
