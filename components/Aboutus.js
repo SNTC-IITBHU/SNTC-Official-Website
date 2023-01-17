a1dcea7273df7040834e10e6b1498b261b04312b
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2023-01-17 22:55:09 +0530

    about done

--- components/Aboutus.js
+++ components/Aboutus.js
@@ -2,17 +2,39 @@ import Wave from "react-wavify";
 
 const Aboutus = () => {
   return (
-    <div>
+    <div className="relative top-[-15%]">
       <Wave
         fill="#2D1634"
         paused={false}
         options={{
-          height: 20,
+          height: 50,
           amplitude: 50,
           speed: 0.35,
-          points: 3,
+          points: 2,
         }}
       />
+      <div className="h-screen relative -top-5 bg-gradient-to-b from-[#2D1634] to-[#1e0925]">
+        <div
+          className="
+        relative top-[20%]
+        flex flex-row w-[90vw]
+        justify-around
+        mx-auto
+        "
+        >
+          <img
+            className="h-[20vh]
+            my-[10%]  
+            "
+            src="/About/imagesntc_abt.svg"
+            alt=""
+          />
+          <div className="about-content flex flex-col justify-around h-[50vh]">
+            <img src="/About/About usabt_title.svg" alt="" />
+            <img src="/About/abt_content.svg" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
