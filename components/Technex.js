e6fb14d4a05fba375203ecfe234f51a13b78bff8
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2023-01-18 15:34:20 +0530

    technex

--- components/Technex.js
+++ components/Technex.js
@@ -1,8 +1,18 @@
+import { AnimationOnScroll } from "react-animation-on-scroll";
+
 
 const Technex=()=>{
     return (
-      <div className="relative top-[-40vh]">
-        <img className="w-screen" src="/Technex/technex.svg" alt="" />
+      <div className="relative flex flex-col top-[-40vh] h-screen w-screen bg-cover bg-[url('/Technex/technex_bg.svg')] justify-around">
+        <img className="h-[30vh] mt-[10vh]" src="/Technex/technex_titile.svg" alt="" />
+        <div className="flex flex-row justify-around">
+          <AnimationOnScroll animateOnce={true} delay={0} animateIn="animate__fadeInLeft">
+            <img src="/Technex/About Technex.svg" alt="" />
+          </AnimationOnScroll>
+          <AnimationOnScroll animateOnce={true} delay={0} animateIn="animate__fadeInRight">
+          <img src="/Technex/content_technex.svg" alt="" />
+          </AnimationOnScroll>
+        </div>
       </div>
     );
 }
