f7630c9c6de10592202fb1b39b6408f6984e00ce
Madhav461 <ygiridhar.madhav.ece21@itbhu.ac.in>
2023-01-18 20:29:14 +0530

    modified:   components/Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,21 @@
 const Footer=()=>{
     return (
-        <div className=" relative  lg:h-screen lg:w-screen lg:bg-cover lg:bg-no-repeat  bg-[url('/Footer/footerwave.svg')] w-screen h-[53vw] bg-no-repeat">
+        <div className=" relative 2xl:h-[30vw] lg:h-screen lg:w-screen lg:bg-cover lg:bg-no-repeat  bg-[url('/Footer/footerwave.svg')] w-screen h-[53vw] bg-no-repeat">
            
            <img src="/Footer/temple.svg" className=" absolute top-[36%] left-[1.6%] 2xl:top-[28%] xl:top-[30%] lg:top-[32%] md:top-[27%] xl:left-[2%] lg:left-[4%] xl:left-[3%] md:left-[4%] xl:w-[20%] lg:w-[22%] md:w-[24%] w-[30%] bg-black"></img>
-           <span className="absolute 2xl:top-[62%] xl:top-[60%] lg:top-[62%] md:top-[65%] 2xl:left-[2.33%] xl:left-[3%] lg:left-[1.3%] md:left-[1.5%] text-white font-medium text-[20px] 2xl:text-[24px] md:text-[16px] text-[9px] top-[79%] left-[0]">Email:gensec.sntc@iitbhu.ac.in</span>
+           <span className="absolute 2xl:top-[79%] xl:top-[60%] lg:top-[62%] md:top-[65%] 2xl:left-[2.33%] xl:left-[3%] lg:left-[1.3%] md:left-[1.5%] text-white font-medium text-[20px] 2xl:text-[24px] md:text-[16px] text-[9px] top-[79%] left-[0]">Email:gensec.sntc@iitbhu.ac.in</span>
            
-           <div className=" absolute 2xl:top-[27%] lg:top-[30%] md:top-[28%] 2xl:left-[28.5%] lg:left-[30%] md:left-[33%] 2xl:h-[41%] lg:h-[37%] md:h-[49%] bg-white xl:w-[0.15%] lg:w-[0.21%] md:w-[0.3%] w-[0.45%] top-[35%] left-[34%] h-[52%]"></div>
+           <div className=" absolute 2xl:top-[27%] lg:top-[30%] md:top-[28%] 2xl:left-[28.5%] lg:left-[30%] md:left-[33%] 2xl:h-[61%] lg:h-[37%] md:h-[49%] bg-white xl:w-[0.15%] lg:w-[0.21%] md:w-[0.3%] w-[0.45%] top-[35%] left-[34%] h-[52%]"></div>
             <img src="/Footer/footerlogo.svg" className="absolute xl:top-[29%] lg:top-[30%] md:top-[26%] xl:left-[35%] lg:left-[33%] md:left-[33%]  xl:w-[28%] lg:w-[33%] md:w-[40%] w-[40%] top-[36%] left-[34.4%]"></img>
-            <span className="absolute 2xl:top-[42.9%] xl:top-[43%] lg:top-[42%] md:top-[44%] 2xl:left-[38%] xl:left-[35.6%] lg:left-[34%] md:left-[36%] text-white font-bold lg:text-[44px] md:text-[34px] text-[16px] top-[54%] left-[37.4%]">Connect with us</span>
-            <img src="/Footer/linkedin.svg" className="absolute hover:cursor-pointer 2xl:top-[54%] xl:top-[51.2%] lg:top-[54%] md:top-[57%] xl:left-[39%] lg:left-[36%] md:left-[39%] xl:w-[4%] lg:w-[5%] top-[65%] left-[38%] w-[7%]"></img>
-            <img src="/Footer/instagram.svg" className="absolute hover:cursor-pointer 2xl:top-[54%] xl:top-[51.2%] lg:top-[54%] md:top-[57%] xl:left-[44%] lg:left-[42%]  md:left-[46%] xl:w-[4%] lg:w-[5%] top-[65%] left-[46%] w-[7%]"></img>
-            <img src="/Footer/facebook.svg" className="absolute hover:cursor-pointer 2xl:top-[54%] xl:top-[51.2%] lg:top-[54%] md:top-[57%] xl:left-[49%] lg:left-[48%] md:left-[53%] xl:w-[4%] lg:w-[5%] top-[65%] left-[54%] w-[7%]"></img>
-            <img src="/Footer/youtube.svg" className="absolute hover:cursor-pointer 2xl:top-[52.3%] xl:top-[49.5%] lg:top-[52.8%] md:top-[55%] xl:left-[54%] lg:left-[54.2%] md:left-[60.2%] xl:w-[5.5%] lg:w-[6%] top-[63.4%] left-[62%] w-[8%]"></img>
-            <div className=" absolute 2xl:top-[27%] lg:top-[30%] md:top-[28%] 2xl:left-[66%] lg:left-[68%] md:left-[72%] 2xl:h-[41%] lg:h-[37%] md:h-[49%] bg-white xl:w-[0.15%] lg:w-[0.15%] md:w-[0.3%] w-[0.3%] top-[35%] right-[28%] h-[52%]"></div>
-            <img src="/Footer/newsletter.svg" className="absolute 2xl:top-[23%] lg:top-[28%] md:top-[28%] xl:right-[6.5%] lg:right-[5%] md:right-[3%] xl:w-[20%] lg:w-[22%] md:w-[23%] w-[21%] top-[40%] right-[3%]"></img>
-            <span className="absolute 2xl:top-[53%] xl:top-[55%] lg:top-[54.5%] md:top-[59%] 2xl:right-[2%] xl:right-[3%] lg:right-[1%] md:right-[0.1%] text-white font-semibold 2xl:text-[34px] lg:text-[23px] md:text-[16px] text-[8px] right-1 top-[70%]">Subscribe to our Newsletter!</span>
-            <button className=" absolute 2xl:top-[60%] xl:top-[62%] lg:top-[61%] md:top-[67%] 2xl:right-[8%] xl:right-[7.5%] lg:right-[8%] md:right-[2%] 2xl:w-[14%] bg-black border-2 rounded-lg  p-1 lg:p-1.5 md:p-1 md:w-[21%]  font-medium text-white opacity-100 top-[76%] right-1 w-[26%] text-[8px]">Enter your email</button>
+            <span className="absolute 2xl:top-[52%] xl:top-[43%] lg:top-[42%] md:top-[44%] 2xl:left-[38%] xl:left-[35.6%] lg:left-[34%] md:left-[36%] text-white font-bold lg:text-[44px] md:text-[34px] text-[16px] top-[54%] left-[37.4%]">Connect with us</span>
+            <img src="/Footer/linkedin.svg" className="absolute hover:cursor-pointer 2xl:top-[66%] xl:top-[51.2%] lg:top-[54%] md:top-[57%] xl:left-[39%] lg:left-[36%] md:left-[39%] xl:w-[4%] lg:w-[5%] top-[65%] left-[38%] w-[7%]"></img>
+            <img src="/Footer/instagram.svg" className="absolute hover:cursor-pointer 2xl:top-[66%] xl:top-[51.2%] lg:top-[54%] md:top-[57%] xl:left-[44%] lg:left-[42%]  md:left-[46%] xl:w-[4%] lg:w-[5%] top-[65%] left-[46%] w-[7%]"></img>
+            <img src="/Footer/facebook.svg" className="absolute hover:cursor-pointer 2xl:top-[66%] xl:top-[51.2%] lg:top-[54%] md:top-[57%] xl:left-[49%] lg:left-[48%] md:left-[53%] xl:w-[4%] lg:w-[5%] top-[65%] left-[54%] w-[7%]"></img>
+            <img src="/Footer/youtube.svg" className="absolute hover:cursor-pointer 2xl:top-[63%] xl:top-[49.5%] lg:top-[52.8%] md:top-[55%] xl:left-[54%] lg:left-[54.2%] md:left-[60.2%] xl:w-[5.5%] lg:w-[6%] top-[63.4%] left-[62%] w-[8%]"></img>
+            <div className=" absolute 2xl:top-[27%] lg:top-[30%] md:top-[28%] 2xl:left-[66%] lg:left-[68%] md:left-[72%] 2xl:h-[61%] lg:h-[37%] md:h-[49%] bg-white xl:w-[0.15%] lg:w-[0.15%] md:w-[0.3%] w-[0.3%] top-[35%] right-[28%] h-[52%]"></div>
+            <img src="/Footer/newsletter.svg" className="absolute 2xl:top-[19%] lg:top-[28%] md:top-[28%] xl:right-[6.5%] lg:right-[5%] md:right-[3%] xl:w-[20%] lg:w-[22%] md:w-[23%] w-[21%] top-[40%] right-[3%]"></img>
+            <span className="absolute 2xl:top-[63%] xl:top-[55%] lg:top-[54.5%] md:top-[59%] 2xl:right-[2%] xl:right-[3%] lg:right-[1%] md:right-[0.1%] text-white font-semibold 2xl:text-[34px] lg:text-[23px] md:text-[16px] text-[8px] right-1 top-[70%]">Subscribe to our Newsletter!</span>
+            <button className=" absolute 2xl:top-[75%] xl:top-[62%] lg:top-[61%] md:top-[67%] 2xl:right-[4%] xl:right-[7.5%] lg:right-[8%] md:right-[2%] 2xl:w-[24%] bg-black border-2 rounded-lg  p-1 lg:p-1.5 md:p-1 md:w-[21%]  font-medium text-white opacity-100 top-[76%] right-1 w-[26%] text-[8px] 2xl:text-[20px]">Enter your email</button>
         </div>
     )
 }
