67880102d1acf643ed4ed78f9773415b9e4aa387
Ayushman Singh <99094157+inviBeast@users.noreply.github.com>
2023-02-11 17:10:18 +0530

    Landing page improvement (#12)
    * Our Clubs In Home page updated 
    
    * Clubs pages main UI Completed

---
+++ public/clubsmenu.js
@@ -0,0 +1,38 @@
+export const clubsMenu=[
+    {
+       id:"AMC",
+       title:"AMC",
+        
+    },
+    {
+        id:"ASTRO",
+        title:"ASTRO",
+         
+     },
+     {
+        id:"BIZ",
+        title:"BIZ",
+         
+     },
+     {
+        id:"COPS",
+        title:"COPS",
+         
+     },
+     {
+        id:"CSI",
+        title:"CSI",
+         
+     },
+     {
+        id:"ROBOTICS",
+        title:"ROBOTICS",
+         
+     },
+     {
+        id:"SAE",
+        title:"SAE",
+         
+     },
+
+];
