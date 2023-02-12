4ef83a2fc648a02f5b7b3a9e912ff28adcd0ce48
ayushmanIITbeast <ayushman.singh.mec21@itbhu.ac.in>
2023-02-12 12:58:07 +0530

    Fixed

--- pages/Ourclubs.js
+++ pages/Ourclubs.js
@@ -1,4 +1,4 @@
-import ClubsPages from "@/components/ClubsPage";
+import ClubsPages from "../components/ClubsPage";
 import Navbar from "../components/Navbar";
 // import Footer from "../components/Footer";
 export default function OurClubs() {
@@ -6,8 +6,8 @@ export default function OurClubs() {
     <>
       <div className="bg-black overflow-hidden border-0 m-0 p-0">
         <div className="pt-[0px] mt-0">
-         <Navbar></Navbar>
-         </div>
+          <Navbar></Navbar>
+        </div>
         <ClubsPages></ClubsPages>
       </div>
     </>
