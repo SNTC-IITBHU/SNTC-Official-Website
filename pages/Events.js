47428396f8177355bf908123615c4c8601f4a3d7
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-07-10 11:23:33 +0530

    added new footer to events page

--- pages/Events.js
+++ pages/Events.js
@@ -1,13 +1,27 @@
 import Navbar from "../components/Navbar";
 import EventsPage from "../components/EventsPage";
-import Footer from "../components/Footer";
+import Footer from "../components/FooterNew";
+import Wave from "react-wavify";
+import {useState} from "react";
 export default function Events() {
-  return (
+    const [isPaused, setIsPaused] = useState(false);
+
+    return (
     <>
      <div className='bg-[#110414]'>
 
         <Navbar></Navbar>
         <EventsPage></EventsPage>
+         <Wave
+             fill="#1E0226"
+             paused={isPaused}
+             options={{
+                 height: 50,
+                 amplitude: 50,
+                 speed: 0.35,
+                 points: 3,
+             }}
+         />
         <Footer></Footer>
      </div>
       
