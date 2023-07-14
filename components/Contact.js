7c4f56d85aac9dc305d41e72363e078a16979e32
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-07-14 16:28:07 +0530

    typo fix

--- components/Contact.js
+++ components/Contact.js
@@ -10,18 +10,6 @@ const Contact = () => {
   const [isPaused, setIsPaused] = useState(true);
   return (
     <div className="relative ">
-      {/* <AnimationOnScroll
-        animateIn="animate__pulse"
-        afterAnimatedIn={function (visible) {
-          setIsPaused(false);
-          setTimeout(() => {
-            setIsPaused(true);
-          }, 1500);
-        }}
-        afterAnimatedOut={function (visible) {
-          setIsPaused(true);
-        }}
-      ></AnimationOnScroll> */}
       <Wave
         fill="#1E0226"
         paused={false}
@@ -32,57 +20,12 @@ const Contact = () => {
           points: 3,
         }}
       />
-      {/*<div className=" h-[70vh] w-screen relative -top-5 flex flex-col justify-around">*/}
       <div className="-mt-4 px-6 md:px-12 xl:px-32 p-8 bg-[#1E0226]">
         <Image
           className="h-[5vh] w-[80vw] mx-auto m-8"
           src={Meet}
           alt=""
         ></Image>
-        {/*<h2 className="mb-4 text-center text-2xl font-bold text-gray-800 dark:text-white md:text-4xl">*/}
-        {/*  Meet our helm*/}
-        {/*</h2>*/}
-        {/* <div className="flex text-center flex-col sm:flex-row justify-around mb-[10vh] bg-contain bg-[url('/Contacts/Soundwaves+rectangle.svg')]">
-          <div className="flex flex-col mt-4 mb-4 items-center sm:max-w-[33vw] mx-1">
-            <Image
-              className="w-[23vw] md:w-auto border-8 rounded-full"
-              src={Img20}
-              alt=""
-            ></Image>
-            <div className="m-auto text-cyan-50 font-bold text-sm">
-             Aditya Kumar Singh
-            </div>
-            <div className="text-white m-auto text-sm">
-              Joint General Secretary
-            </div>
-          </div>
-          <div className="flex flex-col mt-4 mb-4 items-center sm:max-w-[33vw]">
-            <Image
-              className="w-[23vw] md:w-auto border-8 rounded-full"
-              src={Img21}
-              alt=""
-            ></Image>
-            <div className="m-auto text-cyan-50 font-bold text-sm">
-              Aditya Surana
-            </div>
-            <div className="text-white m-auto text-sm">General Secretary</div>
-          </div>
-          <div className="flex flex-col mt-4 mb-4 items-center sm:max-w-[33vw]">
-            <div>
-            <Image
-              className="w-64 cover border-8 rounded-full"
-              src={Img22}
-              alt=""
-            ></Image>
-            </div>
-            <div className="m-auto text-cyan-50 font-bold text-sm">
-              Tanisha Singh
-            </div>
-            <div className="text-white m-auto text-sm">
-              Joint General Secretary
-            </div>
-          </div>
-        </div> */}
         <div className="grid items-center gap-12 md:grid-cols-3 mb-4">
           <div className="space-y-4 text-center">
             <Image
@@ -131,7 +74,7 @@ const Contact = () => {
             />
             <div>
               <h4 className="text-2xl text-gray-700 dark:text-white">
-                Tanish Singh
+                Tanisha Singh
               </h4>
               <span className="block text-sm text-gray-500">
                 Joint General Secretary
