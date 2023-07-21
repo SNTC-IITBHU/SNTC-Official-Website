5be71ce35dedd27cda83cb2ec2230bd9ad1be6d9
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-07-21 09:12:22 +0530

    next image

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -1,32 +1,37 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import Image from "next/image";
+import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 // import "slider.css";
 
 export default function ImageSlider(props) {
-
   return (
-    <Carousel className="flex-coloumn items-center justify-center sm:w-[50rem]" thumbWidth={90} autoPlay={true} infiniteLoop showArrows={false} showStatus={false} showThumbs={false} interval={2000} height="40rem" >
+    <Carousel
+      className="flex-coloumn items-center justify-center sm:w-[50rem]"
+      thumbWidth={90}
+      autoPlay={true}
+      infiniteLoop
+      showArrows={false}
+      showStatus={false}
+      showThumbs={false}
+      interval={2000}
+      height="40rem"
+    >
       <div>
-        <img src={props.slides[0].source} />
-
+        <Image alt={""} width={100} height={100} src={props.slides[0].source} />
       </div>
       <div>
-        <img src={props.slides[1].source} />
-
+        <Image alt={""} width={100} height={100} src={props.slides[1].source} />
       </div>
       <div>
-        <img src={props.slides[2].source} />
-
+        <Image alt={""} width={100} height={100} src={props.slides[2].source} />
       </div>
       <div>
-        <img src={props.slides[3].source} />
-
+        <Image alt={""} width={100} height={100} src={props.slides[3].source} />
       </div>
     </Carousel>
   );
 }
-
 
 // ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
