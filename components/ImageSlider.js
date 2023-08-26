9ffc485e841e1c1b806181f0105aca9f90b903fb
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-08-27 01:39:25 +0530

    Removed reduncy in Navbar.js & Clubspage.js Also, improved ImageSlider.js by making the render dynamic. One bug still persists. When the tab is reloaded, the carousel shifts a bit upwards and then a bit downwards, probably a reduncy in the css file

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -1,37 +1,28 @@
-import React, { Component } from "react";
 import Image from "next/image";
-import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-// import "slider.css";
+// import "slider.css"
 
-export default function ImageSlider(props) {
-  return (
-    <Carousel
-      className="flex-coloumn items-center justify-center sm:w-[50rem]"
-      thumbWidth={90}
-      autoPlay={true}
-      infiniteLoop
-      showArrows={false}
-      showStatus={false}
-      showThumbs={false}
-      interval={2000}
-      height="40rem"
-    >
-      <div>
-        <Image alt={""} width={100} height={100} src={props.slides[0].source} />
-      </div>
-      <div>
-        <Image alt={""} width={100} height={100} src={props.slides[1].source} />
-      </div>
-      <div>
-        <Image alt={""} width={100} height={100} src={props.slides[2].source} />
-      </div>
-      <div>
-        <Image alt={""} width={100} height={100} src={props.slides[3].source} />
-      </div>
-    </Carousel>
-  );
+export default function ImageSlider({ slides }) {
+    return (
+        <Carousel
+            className="flex-coloumn items-center justify-center sm:w-[50rem]"
+            thumbWidth={90}
+            autoPlay={true}
+            infiniteLoop
+            showArrows={false}
+            showStatus={false}
+            showThumbs={false}
+            interval={2000}
+            height="40rem"
+        >
+            {slides.map((slide, index) => (
+                <div key={index}>
+                    <Image alt={slide.title} width={100} height={100} src={slide.source} />
+                </div>
+            ))}
+        </Carousel>
+    );
 }
 
 // ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
