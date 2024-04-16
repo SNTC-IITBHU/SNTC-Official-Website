30dc7fef553f579a10d4f58d52402fa574a93f93
Nandini Arora <nandini.arora.che22@itbhu.ac.in>
2024-04-16 06:28:38 +0530

    working navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,69 +10,75 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const [active, setActive] = useState("Home");
 
+  const toggleMenu = () => {
+    setToggle(!toggle);
+  };
+
+  const handleNavLinkClick = (title) => {
+    setActive(title);
+    setToggle(false); // Close the mobile menu when a link is clicked
+  };
+
   return (
-    <div className="flex z-190 bg-[#110414]">
-      <div className="sm:fixed w-[40%] p-[0%] z-[190] sm:p-0 2xl:w-[20%] xl:w-[18%] lg:w-[18%] md:w-[20%] sm:w-[20%] opacity-100 m-1">
+    <div className="flex z-50 bg-[#110414]">
+      {/* Sidebar Logo */}
+      <div className="sm:fixed w-[40%] p-[0%] sm:p-0 2xl:w-[20%] xl:w-[18%] lg:w-[18%] md:w-[20%] sm:w-[20%] opacity-100 m-1">
         <Link href={"/"}>
           <Image
             src={Sidelogo}
             alt="Logo"
-            className="z-[190] w-[50%] 2xl:w-[40%] mt-2 2xl:ml-4 lg:ml-3"
+            className="w-[50%] 2xl:w-[40%] mt-2 2xl:ml-4 lg:ml-3"
           />
         </Link>
       </div>
 
-      <div className="fixed bg-[#110414] right-0 opacity-100 z-[180] w-[85%] sm:flex hidden 2xl:w-[80%] xl:w-[83%] lg:w-[80%] md:w-[90%] pl-0 md:pl-[24%] sm:w-[80%]">
-        <div className="w-screen bg-[#110414] fixed right-0 2xl:h-[9vh] lg:h-[10.5vh] h-[6.5vh]"></div>
-        <div className="fixed z-[100] top-0 right-0">
-          <ul className="list-none sm:flex hidden z-[100] justify-end items-center flex-1">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`mt-1 lg:mt-4 font-poppins font-normal tracking-wider cursor-pointer z-[100]  2xl:text-[22px] xl:text-[20px] lg:text-[16px] md:text-[14px] sm:text-[12px] 2xl:mt-[4%] ${
-                  active === nav.title
-                    ? ` z-[100] text-white font-bold`
-                    : "text-stone-300"
-                } ${
-                  index === navLinks.length - 1
-                    ? "2xl:mr-[8%] xl:mr-[8%] lg:mr-[6%] md:mr-[2%] sm:mr-[5%]"
-                    : "2xl:mr-[14%] xl:mr-[14%] lg:mr-[10%] md:mr-[3%] sm:mr-[6%]"
-                }`}
-                onClick={() => setActive(nav.title)}
-              >
-                <Link href={`${nav.id}`}>{nav.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+      {/* Desktop Navigation */}
+      <div className="hidden sm:flex bg-[#110414] z-[100] justify-end items-center flex-1">
+        <ul className="list-none flex z-[100] justify-end items-center">
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`mt-1 lg:mt-4 font-poppins font-normal tracking-wider cursor-pointer ${
+                active === nav.title ? "text-white font-bold" : "text-stone-300"
+              } ${
+                nav === navLinks[navLinks.length - 1]
+                  ? "mr-[8%]"
+                  : "mr-[14%]"
+              }`}
+              onClick={() => setActive(nav.title)}
+            >
+              <Link href={nav.id}>{nav.title}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
 
+      {/* Mobile Navigation */}
       <div className="sm:hidden flex flex-1 justify-end items-center z-[200] pr-[2%] pt-0">
         <Image
           src={toggle ? Close : Menu}
           alt="menu"
-          className="fixed top-2 right-3 w-[30px] h-[34px] object-contain opacity-100 z-[100]"
-          onClick={() => setToggle(!toggle)}
+          className="w-[30px] h-[34px] object-contain cursor-pointer"
+          onClick={toggleMenu}
         />
 
-        <div
-          className={`${
-            !toggle ? "hidden" : "flex "
-          }  p-6 bg-[#240631] opacity-100 fixed top-[7%] right-0 mx-4 my-0 min-w-[140px] rounded-xl sidebar z-[900] `}
-        >
-          <ul className="list-none opacity-100 flex justify-end items-start flex-1 flex-col z-[100]">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[14px] z-[100] ${
-                  active === nav.title ? "text-white" : "text-stone-300"
-                } ${index === navLinks.length - 1 ? "mb-0" : "mb-2"}`}
-                onClick={() => setActive(nav.title)}
-              >
-                <Link href={`#${nav.id}`}>{nav.title}</Link>
-              </li>
-            ))}
-          </ul>
+        {/* Mobile Menu */}
+        <div className={`absolute top-0 right-0 ${toggle ? "block" : "hidden"}`}>
+          <div className="bg-[#240631] p-6 rounded-xl">
+            <ul className="list-none">
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[14px] ${
+                    active === nav.title ? "text-white" : "text-stone-300"
+                  } ${nav === navLinks[navLinks.length - 1] ? "" : "mb-2"}`}
+                  onClick={() => handleNavLinkClick(nav.title)}
+                >
+                  <Link href={nav.id}>{nav.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
